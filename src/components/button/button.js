8cc62a4cacfbf4ffc8dd9aad18d46b8f30008fad
ARG <ankit.g@antino.com>
2024-02-23 18:12:26 +0530

    button updated

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,17 +3,13 @@ import './button.scss';
 import { Button } from 'antd';
 import Editicon from '../../assets/editButton.svg';
 import Deleteicon from '../../assets/deleteButton.svg';
-import RightArrow from '../../assets/Right.svg';
+import Arrow from '../../assets/Arrow.svg';
+import RightArrow from '../../assets/RightArrow.svg';
+import LeftArrow from '../../assets/LeftArrow.svg';
+import Filter from '../../assets/Filter.svg';
 
-
-const ButtonComponent = ({ title, onClick, disabled, type, className, isSubmit, size }) => {
-
-	const handleClick = () => {
-		if (onClick) {
-			onClick();
-		}
-	};
-
+const ButtonComponent = ({ title, onClick, disabled, type, className, isSubmit, ...rest}) => {
+	
 	let buttonContent;
 
 	switch (type) {
@@ -23,8 +19,17 @@ const ButtonComponent = ({ title, onClick, disabled, type, className, isSubmit, 
 		case 'delete':
 			buttonContent = <img src={Deleteicon} alt="Delete Icon" />;
 			break;
+		case 'arrow':
+			buttonContent = <img src={Arrow} alt="Arrow Icon" />;
+			break;
 		case 'rightArrow':
-			buttonContent = <img src={RightArrow} alt="Delete Icon" />;
+			buttonContent = <img src={RightArrow} alt="RightArrow Icon" />;
+			break;
+		case 'leftArrow':
+			buttonContent = <img src={LeftArrow} alt="LeftArrow Icon" />;
+			break;
+		case 'filter':
+			buttonContent = <img src={Filter} alt="Filter Icon" />;
 			break;
 		default:
 			buttonContent = title;
@@ -32,10 +37,11 @@ const ButtonComponent = ({ title, onClick, disabled, type, className, isSubmit, 
 
 	return (
 		<Button
-			className={`button ${size} ${className}`}
+			className={`button ${className}`}
 			htmlType={isSubmit ? "submit" : "button"}
-			onClick={handleClick}
+			onClick={onClick}
 			disabled={disabled}
+			{...rest}
 		>
 			{buttonContent}
 		</Button>
