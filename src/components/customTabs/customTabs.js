f5175fd81e1eb48fe45788202cd8ea9e20cc825c
Jitesh_Shivgan <jitesh.s@antino.com>
2024-02-23 14:41:33 +0530

    Made card type tabs common component too

--- src/components/customTabs/customTabs.js
+++ src/components/customTabs/customTabs.js
@@ -1,7 +1,20 @@
 import { Tabs } from 'antd';
 import React from 'react';
+import './customTabs.scss';
 
-const CustomTabs = ({ defaultActiveKey, items, onChange }) => {
+const CustomTabs = ({ defaultActiveKey, items, onChange, type }) => {
+	if (type === 'card') {
+		return (
+			<Tabs defaultActiveKey={defaultActiveKey} onChange={onChange} type="card" className="custom_tabs_card">
+				{items.map((item) => (
+					<Tabs.TabPane tab={item.label} key={item.key}>
+						{item.children}
+					</Tabs.TabPane>
+				))}
+			</Tabs>
+		);
+	}
+
 	return (
 		<Tabs defaultActiveKey={defaultActiveKey} onChange={onChange}>
 			{items.map((item) => (
