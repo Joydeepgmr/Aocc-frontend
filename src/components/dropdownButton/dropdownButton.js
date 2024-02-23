8cac695daab8304969c9f31e50c0a9387efc4114
ARG <ankit.g@antino.com>
2024-02-23 18:13:32 +0530

    updated dropdowncomponent component

--- src/components/dropdownButton/dropdownButton.js
+++ src/components/dropdownButton/dropdownButton.js
@@ -4,25 +4,18 @@ import { Dropdown, Space } from 'antd';
 import './dropdownButton.scss';
 
 
-const DropdownButton = ({ dropdownItems, buttonText, className }) => {
-  
+const DropdownButton = ({ dropdownItems, buttonText, className, ...rest }) => {
+
   return (
-  <Dropdown
-    menu={{
-      items: dropdownItems,
-    }}
-    trigger={['click']}
-  >
-    <a onClick={(e) =>
-      e.preventDefault()}
-      className={`create ${className}`}
+    <Dropdown menu={{ items: dropdownItems }} trigger={['click']} >
+      <a onClick={(e) =>
+        e.preventDefault()}
+        className={`create ${className}`}
+        {...rest}
       >
-      <Space>
-        {buttonText}
-        <img src={downOutLined} />
-      </Space>
-    </a>
-  </Dropdown>
+        <Space>{buttonText}<img src={downOutLined}/></Space>
+      </a>
+    </Dropdown>
   );
 };
 
