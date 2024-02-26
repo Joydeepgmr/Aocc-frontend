85379634e40f80f5d90dc79708bcc20a4d170696
Sameer Rana <Sameer.r@antino.com>
2024-02-26 14:08:08 +0530

    saving progress on common component

---
+++ src/components/table/table.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Table, Empty } from 'antd';
+import './table.scss';
+
+const TableComponent = ({
+    columns,
+    data = [],
+    loading = false,
+    onChange = () => {}, 
+    tableTitle = '', 
+    emptyText = 'No data available' 
+}) => {
+
+    // Function to handle table change events (pagination, filtering, sorting)
+    const handleTableChange = (pagination, filters, sorter) => {
+        // Extract sortField and sortOrder from sorter object
+        const sortField = sorter?.field;
+        const sortOrder = sorter?.order;
+        
+        // Call onChange prop with updated table state
+        onChange({
+            sortField,
+            sortOrder,
+            pagination,
+            ...filters,
+        });
+    };
+
+    return (
+        <div className='table-container'>
+            {tableTitle && <h3 className='table-title'>{tableTitle}</h3>}
+            <Table
+                columns={columns}
+                dataSource={data}
+                loading={loading}
+                onChange={handleTableChange}
+                locale={{
+                    emptyText: <Empty description={emptyText} />
+                }}
+            />
+        </div>
+    );
+};
+
+TableComponent.propTypes = {
+    columns: PropTypes.array.isRequired,
+    data: PropTypes.array,
+    loading: PropTypes.bool,
+    onChange: PropTypes.func,
+    tableTitle: PropTypes.string,
+    emptyText: PropTypes.string
+};
+
+export default TableComponent;
