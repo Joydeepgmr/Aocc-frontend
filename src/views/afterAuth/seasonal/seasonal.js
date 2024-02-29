6c55f31e25f1bf3bbe5aad4c42f803db2c19e743
ARG <ankit.g@antino.com>
2024-02-29 12:15:48 +0530

    upadated seasonal section

--- src/views/afterAuth/seasonal/seasonal.js
+++ src/views/afterAuth/seasonal/seasonal.js
@@ -3,39 +3,40 @@ import Button from '../../../components/button/button';
 import ModalComponent from '../../../components/modalComponent/modalComponent';
 import './seasonal.scss';
 import FormComponent from '../formComponent/formComponent';
+import UploadCsvModal from '../../../components/uploadCsvModal/uploadCsvModal';
+
 
 
 const Seasonal = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isCsvModalOpen, setIsCsvModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
     const openCsvModal = () => {
         setIsCsvModalOpen(true);
     };
+
     const closeModal = () => {
         setIsModalOpen(false);
         setIsCsvModalOpen(false);
     };
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
+
     return (
-        <div className="mainHead">
+        <div className="main_Head">
             <div className="container">
                 <Button title="Create" id="btn" type="filledText" isSubmit="submit" onClick={openModal} />
-                <ModalComponent isModalOpen={isModalOpen} width="120rem" closeModal={closeModal} title="Seasonal Planning" id="modals_class">
-                    <div className="modal-content">
+                <ModalComponent isModalOpen={isModalOpen} width="120rem" closeModal={closeModal} title="Seasonal Planning" className="custom_modal">
+                    <div className="modal_content">
                         <FormComponent />
                     </div>
                 </ModalComponent>
 
-                <Button
-                    id="btn"
-                    title="Upload CSV"
-                    className="custom_svgButton"
-                    type="filledText"
-                    isSubmit="submit"
-                    onClick={openCsvModal}
+                <Button id="btn" title="Upload CSV" className="custom_svgButton" type="filledText" isSubmit="submit" onClick={openCsvModal}
                 />
+                <UploadCsvModal isModalOpen={isCsvModalOpen} width="720px" closeModal={closeModal} />
                 <Button
                     id="btn"
                     title="Download CSV Template"
