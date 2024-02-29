9047e79edef8220aab1286edd38494d2d790ba0d
Sameer Rana <Sameer.r@antino.com>
2024-02-29 10:47:46 +0530

    added tabs for within Flight schedule

--- src/views/afterAuth/dashboard/components/FlightSchedule.jsx
+++ src/views/afterAuth/dashboard/components/FlightSchedule.jsx
@@ -4,10 +4,26 @@ import CustomTabs from '../../../../components/customTabs/customTabs';
 import TableComponent from '../../../../components/table/table';
 import InputField from '../../../../components/inputField/inputField';
 import Button from '../../../../components/button/button';
-import Filter from "../../../../assets/Filter.svg"
+import Filter from '../../../../assets/Filter.svg'
 import './style.scss';
 
 const FlightSchedule = () => {
+	const items = [
+		{
+			key: '1',
+			label: 'Arrival',
+			children: <><h1>Hello 1</h1></>,
+		},
+		{
+			key: '2',
+			label: 'Departure',
+			children: <><h1>Hello 2</h1></>,
+		},
+	];
+	const handleChange = () => {
+		console.log('Tab switch');
+	};
+
   return (
     <div className='body-container'>
        <div className='top-bar'>
@@ -30,6 +46,9 @@ const FlightSchedule = () => {
 
         </div>
         </div> 
+		<div className='table-container'>
+		<CustomTabs defaultActiveKey="1" items={items} onChange={handleChange} />
+		</div>
     </div>
   )
 }
