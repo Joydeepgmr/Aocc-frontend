1a13aa701a3a3e87ab226a71eaef12fcee3d6d44
Jitesh_Shivgan <jitesh.s@antino.com>
2024-02-29 11:14:36 +0530

    Css bug resolved

--- src/components/datapicker/datepicker.js
+++ src/components/datapicker/datepicker.js
@@ -1,23 +1,49 @@
+import { DatePicker, Form } from 'antd';
+import dayjs from 'dayjs';
 import React from 'react';
-import { DatePicker } from 'antd';
 import './datepicker.scss';
-
-const Date = ({ label, placeholder, dateFormat = 'DD/MM/YYYY', handleChange,  ...rest }) => {
-  const handleDateChange = (date,dateString) => {		
-		handleChange(dateString);
-  };
+const Date = ({
+	label,
+	name,
+	required,
+	width,
+	disabled,
+	placeholder,
+	disabledDate,
+	isDisabledDate = false,
+	acceptAllDate,
+	format,
+	className,
+}) => {
 	return (
-		<div {...rest}>
-			<div className="dateStyle">
-				<label for="date">{label}</label>
-				<DatePicker
-					className="DateStyle"
-					placeholder={placeholder}
-					format={dateFormat}
-					onChange={handleDateChange}
-				/>
-			</div>
-		</div>
+		<Form.Item
+			label={label}
+			name={name}
+			className={`${className} date_form_item`}
+			rules={[
+				{
+					required: required ? true : false,
+					message: 'This field is required.',
+				},
+			]}
+		>
+			<DatePicker
+				placeholder={placeholder}
+				className="date_style"
+				format={format}
+				disabledDate={(current) => {
+					if (acceptAllDate) {
+						return false;
+					} else if (isDisabledDate) {
+						return disabledDate(current);
+					} else {
+						let customDate = dayjs().format('YYYY/MM/DD');
+						return current && current > dayjs(customDate, 'YYYY/MM/DD');
+					}
+				}}
+				disabled={disabled ? true : false}
+			/>
+		</Form.Item>
 	);
 };
 
