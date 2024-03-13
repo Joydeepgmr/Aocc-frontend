72d8a482732ce87a2c0299f0bb6d042cd222c747
Sameer Rana <Sameer.r@antino.com>
2024-03-13 08:17:10 +0530

    code refactoring

--- src/views/beforeAuth/login/login.js
+++ src/views/beforeAuth/login/login.js
@@ -64,7 +64,7 @@ export const Login = () => {
 					navigate(Pathname.DASHBOARD);
 					break;
 				default:
-					// Handle default case or invalid role
+					navigate(Pathname.DASHBOARD);
 					break;
 			}
 		}
