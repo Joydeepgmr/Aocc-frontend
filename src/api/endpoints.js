488ff558da605f91862471cd930d1524bbc13c92
shaluAntino <shalu@antino.com>
2024-03-19 21:52:17 +0530

    integrated get, post and patch api for Seasonal Plan Arrival and Departure, Created a resuable WeeklySelect Component

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -5,3 +5,9 @@ export const DUMMY = '/dummy';
 
 export const GET_ALL_TIMELINE_DATA = '/resource-allocation/get-allocated-resources';
 export const GET_TIMELINE_GROUP_DATA = '/resource-allocation/get-resources';
+
+/*------------------ Seasonal Plans --------------------- */
+export const GET_SEASONAL_PLANS = '/seasonalPlan/getPlans';
+export const POST_SEASONAL_PLANS = '/seasonalPlan/createPlan';
+export const EDIT_SEASONAL_PLANS_ARRIVAL = '/seasonalPlan/arrival';
+export const EDIT_SEASONAL_PLANS_DEPARTURE = 'seasonalPlan/departure';
---
+++ src/services/SeasonalPlanServices/seasonalPlan.js
@@ -0,0 +1,79 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { GET_SEASONAL_PLANS, POST_SEASONAL_PLANS, EDIT_SEASONAL_PLANS_ARRIVAL, EDIT_SEASONAL_PLANS_DEPARTURE } from '../../api';
+import { Get, Post,Patch } from '../HttpServices/HttpServices';
+
+export const useGetSeasonalPlans = (flightType,props) => {
+	const response = useQuery({
+		queryKey: ['get-seasonal-plans', flightType],
+		queryFn: async () => await Get(`${GET_SEASONAL_PLANS}?flightType=${flightType}`),
+		...props,
+	});
+
+	const { data, error, isSuccess } = response;
+
+	const statusMessage = isSuccess ? data?.message : error?.message;
+
+	return {
+		...response,
+		data: data,
+		message: statusMessage,
+	};
+};
+
+export const usePostSeasonalPlans = (props) => {
+    const queryClient = useQueryClient()
+	const response = useMutation({
+		mutationKey: ['post-seasonal-plans'],
+		mutationFn: async (data) => await Post(`${POST_SEASONAL_PLANS}`, data),
+        onSuccess: () => {
+			queryClient.invalidateQueries('get-seasonal-plans');
+		},
+		...props,
+	});
+
+	const { data, error, isSuccess } = response;
+
+	const statusMessage = isSuccess
+		? data?.message
+		: error?.response?.data?.data?.message ?? error?.response?.data?.data?.error;
+
+	return { ...response, data, message: statusMessage };
+};
+
+export const useEditSeasonalPlanArrival = (id,props) => {
+	const queryClient = useQueryClient();
+	const response = useMutation({
+		mutationKey: ['edit-seasonal-plan-arrival'],
+		mutationFn: (data) => Patch(`${EDIT_SEASONAL_PLANS_ARRIVAL}/${id}`, data),
+		onSuccess: () => {
+			queryClient.invalidateQueries('get-seasonal-plans');
+		},
+		...props,
+	});
+	const { data, error, isSuccess } = response;
+
+	const statusMessage = isSuccess
+		? data?.data?.message
+		: error?.response?.data?.data?.error ?? error?.response?.data?.data?.message;
+
+	return { ...response, data: data?.data, message: statusMessage };
+};
+
+export const useEditSeasonalPlanDeparture = (id,props) => {
+	const queryClient = useQueryClient();
+	const response = useMutation({
+		mutationKey: ['edit-seasonal-plan-departure'],
+		mutationFn: (data) => Patch(`${EDIT_SEASONAL_PLANS_DEPARTURE}/${id}`, data),
+		onSuccess: () => {
+			queryClient.invalidateQueries('get-seasonal-plans');
+		},
+		...props,
+	});
+	const { data, error, isSuccess } = response;
+
+	const statusMessage = isSuccess
+		? data?.data?.message
+		: error?.response?.data?.data?.error ?? error?.response?.data?.data?.message;
+
+	return { ...response, data: data?.data, message: statusMessage };
+};
