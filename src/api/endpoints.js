dd0b04fb58eefd3b15b9aa493150c312d1609595
Sameer Rana <Sameer.r@antino.com>
2024-03-19 10:57:18 +0530

    Login api integration

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,2 +1,7 @@
-export const USER_LOGIN = '/login';
 export const DUMMY = '/dummy';
+
+/*------------- login --------------*/
+export const USER_LOGIN = 'user/login';
+
+/*------------- IT admin airports --------------*/
+export const GLOBAL_AIRPORTS = "" 
