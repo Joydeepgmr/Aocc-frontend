774d4536808aa1c6047696525b999be2f537745a
Shashank Yadav <shashank.5060@gmail.com>
2024-03-22 14:47:54 +0530

    fix missing file and typo

--- src/views/afterAuth/plans/components/CDM/components/arrival/arrival.js
+++ src/views/afterAuth/plans/components/CDM/components/arrival/arrival.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './arrival.scss';
+// import './arrival.scss';
 import TableComponent from '../../../../../../../components/table/table';
 
-const Arrival = ({data,columns}) => {
+const Arrival = ({ data, columns }) => {
 
     const handleTableChange = (pagination, filters, sorter) => {
-		console.log('Table changed:', pagination, filters, sorter);
-	};
+        console.log('Table changed:', pagination, filters, sorter);
+    };
 
     return (
         <div className="main">
-            <div>    
-				<TableComponent columns={columns} data={data} onChange={handleTableChange} />
-			</div>
+            <div>
+                <TableComponent columns={columns} data={data} onChange={handleTableChange} />
+            </div>
         </div>
     );
 };
--- src/views/afterAuth/plans/components/CDM/components/departure/departure.js
+++ src/views/afterAuth/plans/components/CDM/components/departure/departure.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import './departure.scss';
+// import './departure.scss';
 import TableComponent from '../../../../../../../components/table/table';
 
-const Departure = ({data, columns}) => {
-    
+const Departure = ({ data, columns }) => {
+
     const handleTableChange = (pagination, filters, sorter) => {
-		console.log('Table changed:', pagination, filters, sorter);
-	};
+        console.log('Table changed:', pagination, filters, sorter);
+    };
 
     return (
         <div className="main">
             <div>
-				<TableComponent columns={columns} data={data} onChange={handleTableChange} />
-			</div>
+                <TableComponent columns={columns} data={data} onChange={handleTableChange} />
+            </div>
         </div>
     );
 };
