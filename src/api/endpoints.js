fecc04e51ecb1e1c959d5b1e875c01fd4ff8832b
Sameer Rana <Sameer.r@antino.com>
2024-03-26 11:15:06 +0530

    changed the endpoint for getting licensing airport names

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -11,7 +11,7 @@ export const USER_LOGIN = 'user/login';
 /*------------- IT admin airport Master --------------*/
 export const POST_LICENSE = "airport-license" 
 export const GET_LICENSE = "airport-license/"
-export const GET_AIRPORT_NAME = "airport/"
+export const GET_AIRPORT_NAME = "global-airport/"
 
 /*------------------ Seasonal Plans --------------------- */
 export const GET_SEASONAL_PLANS = '/seasonalPlan/getPlans';
