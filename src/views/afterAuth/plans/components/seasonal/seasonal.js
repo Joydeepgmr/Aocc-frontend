0e239cd040cfa349b8a276f2cf448d875b3a4e03
anjali123dmsmflsm <anjali@antino.com>
2024-04-05 15:40:10 +0530

    integrate download API and also download csv upload

--- src/views/afterAuth/plans/components/seasonal/seasonal.js
+++ src/views/afterAuth/plans/components/seasonal/seasonal.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQueryClient } from 'react-query';
 import dayjs from 'dayjs';
 import toast from 'react-hot-toast';
@@ -16,11 +16,18 @@ import Arrival from './components/arrival/arrival';
 import Departure from './components/departure/departure';
 import editIcon from '../../../../../assets/logo/edit.svg';
 import { ConvertUtcToIst, ConvertIstToUtc } from '../../../../../utils';
-import { useEditSeasonalPlanArrival, useGetSeasonalPlans, usePostSeasonalPlans, useEditSeasonalPlanDeparture, useUploadCSV } from '../../../../../services/SeasonalPlanServices/seasonalPlan';
+import {
+	useEditSeasonalPlanArrival,
+	useGetSeasonalPlans,
+	usePostSeasonalPlans,
+	useEditSeasonalPlanDeparture,
+	useUploadCSV,
+	useDownloadCSV,
+} from '../../../../../services/SeasonalPlanServices/seasonalPlan';
 
 import './seasonal.scss';
 
-const Seasonal = ({tab}) => {
+const Seasonal = ({ tab }) => {
 	const queryClient = useQueryClient();
 	const [seasonalData, setSeasonalData] = useState([]);
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -40,15 +47,20 @@ const Seasonal = ({tab}) => {
 			const newData = data.pages.reduce((acc, page) => {
 				return acc.concat(page.data?.flightSchedule || []);
 			}, []);
-		
+
 			setSeasonalData([...newData]);
 		}
 	};
 
 	const handleGetSeasonalError = (error) => {
 		toast.error(error?.response?.data?.message);
-	}
-	const { data: fetchedSeasonalPlans, isLoading: isFetchLoading, hasNextPage, fetchNextPage } = useGetSeasonalPlans(flightType,tab,getSeasonalHandler);
+	};
+	const {
+		data: fetchedSeasonalPlans,
+		isLoading: isFetchLoading,
+		hasNextPage,
+		fetchNextPage,
+	} = useGetSeasonalPlans(flightType, tab, getSeasonalHandler);
 
 	const openModal = () => {
 		setIsModalOpen(true);
@@ -85,6 +97,8 @@ const Seasonal = ({tab}) => {
 			openModal();
 		} else if (value === 'uploadCSV') {
 			openCsvModal();
+		} else if (value === 'downloadCSVTemplate') {
+			handleDownloadCSV();
 		}
 	};
 
@@ -95,7 +109,7 @@ const Seasonal = ({tab}) => {
 	};
 
 	const handleAddSeasonalSuccess = (data) => {
-		setSeasonalData([])
+		setSeasonalData([]);
 		closeModal();
 		toast.success(data?.message);
 		queryClient.invalidateQueries('get-seasonal-plans');
@@ -128,12 +142,12 @@ const Seasonal = ({tab}) => {
 		setIsEditModalOpen(false);
 	};
 
-	//EDIT 
+	//EDIT
 	const handleEdit = (record) => {
 		record = {
 			...record,
 			date: record?.PDATE ? dayjs(record?.PDATE) : '',
-		}
+		};
 		setRowData(record);
 		openEditModal();
 	};
@@ -141,21 +155,24 @@ const Seasonal = ({tab}) => {
 	const handleSeasonalEditSuccess = (data) => {
 		closeEditModal();
 		setSeasonalData([]);
-		toast.success(data?.message);	
-		queryClient.invalidateQueries('get-seasonal-plans');	
-	}
+		toast.success(data?.message);
+		queryClient.invalidateQueries('get-seasonal-plans');
+	};
 
 	const handleSeasonalEditError = (error) => {
 		toast.error(error?.response?.data?.message);
-	}
+	};
 
 	const editSeasonalPlansHandler = {
-		onSuccess: (data) =>handleSeasonalEditSuccess(data),
+		onSuccess: (data) => handleSeasonalEditSuccess(data),
 		onError: (error) => handleSeasonalEditError(error),
 	};
-	
-	const {mutate: editSeasonalPlanArrival, isLoading: isEditLoading} = useEditSeasonalPlanArrival(rowData?.id,editSeasonalPlansHandler)
-	const {mutate: editSeasonalPlanDeparture} = useEditSeasonalPlanDeparture(rowData?.id, editSeasonalPlansHandler)
+
+	const { mutate: editSeasonalPlanArrival, isLoading: isEditLoading } = useEditSeasonalPlanArrival(
+		rowData?.id,
+		editSeasonalPlansHandler
+	);
+	const { mutate: editSeasonalPlanDeparture } = useEditSeasonalPlanDeparture(rowData?.id, editSeasonalPlansHandler);
 	const handleEditSave = (value) => {
 		const data = {
 			FLIGHTNO: value.FLIGHTNO,
@@ -167,9 +184,9 @@ const Seasonal = ({tab}) => {
 			pos: value.pos,
 			PDATE: ConvertIstToUtc(value.date),
 			registration: value.registration,
-		}
+		};
 		index === '1' && editSeasonalPlanArrival(data);
-		index=== '2' && editSeasonalPlanDeparture(data);
+		index === '2' && editSeasonalPlanDeparture(data);
 	};
 
 	const dropdownItems = [
@@ -200,22 +217,22 @@ const Seasonal = ({tab}) => {
 			/>
 		</div>
 	);
-	
+
 	const handleUploadCsvSuccess = () => {
 		queryClient.invalidateQueries('get-seasonal-plans');
 		closeCsvModal();
-	}
+	};
 
 	const handleUploadCsvError = () => {
-		toast.error("Invalid File Type");
-	}
-	
+		toast.error('Invalid File Type');
+	};
+
 	const uploadCsvHandler = {
 		onSuccess: () => handleUploadCsvSuccess(),
 		onError: () => handleUploadCsvError(),
 	};
 
-	const {mutate: onUploadCSV} = useUploadCSV(uploadCsvHandler);
+	const { mutate: onUploadCSV } = useUploadCSV(uploadCsvHandler);
 
 	//UPLOAD
 	const handleUpload = (file) => {
@@ -226,35 +243,67 @@ const Seasonal = ({tab}) => {
 		} else {
 			console.error('No file provided for upload.');
 		}
-	}
-	
+	};
+
+	const onError = ({
+		response: {
+			data: { message },
+		},
+	}) => toast.error(message);
+
+	const { refetch, isLoading: isDownloading } = useDownloadCSV({ onError });
+
+	//DOWNLOAD
+	const handleDownloadCSV = () => {
+		refetch();
+	};
 
 	const columns = [
 		{
 			title: 'Flight No.',
 			dataIndex: 'FLIGHTNO',
 			key: 'FLIGHTNO',
-			render: (FLIGHTNO) => (FLIGHTNO ?? '-'),
+			render: (FLIGHTNO) => FLIGHTNO ?? '-',
 		},
-		{ title: 'Date', dataIndex: 'PDATE', key: 'PDATE', render: (PDATE) => (PDATE !== null ? ConvertUtcToIst(PDATE) : '-') },
+		{
+			title: 'Date',
+			dataIndex: 'PDATE',
+			key: 'PDATE',
+			render: (PDATE) => (PDATE !== null ? ConvertUtcToIst(PDATE) : '-'),
+		},
 		{
 			title: 'Call Sign',
 			dataIndex: 'callSign',
 			key: 'callSign',
-			render: (callSign) => (callSign ?? '-'),
+			render: (callSign) => callSign ?? '-',
 		},
 		{
 			title: 'Nature Code',
 			dataIndex: 'natureCode',
 			key: 'natureCode',
-			render: (natureCode) => (natureCode ?? '-'),
+			render: (natureCode) => natureCode ?? '-',
 		},
-		{ title: 'ORG', dataIndex: 'origin', key: 'origin', render: (origin) => (origin ?? '-') },
+		{ title: 'ORG', dataIndex: 'origin', key: 'origin', render: (origin) => origin ?? '-' },
 		index === '1'
-			? { title: 'STA', dataIndex: 'STA', key: 'STA', render: (STA) => (STA !== null ? (STA)?.split('T')[1].slice(0,5) : '-') }
-			: { title: 'STD', dataIndex: 'STD', key: 'STD', render: (STD) => (STD !== null ? (STD)?.split('T')[1].slice(0,5) : '-'), },
-		{ title: 'POS', dataIndex: 'pos', key: 'pos', render: (pos) => (pos ?? '-'), },
-		{ title: 'REG No.', dataIndex: 'registration', key: 'registration', render: (registration) => ( registration ?? '-'), },
+			? {
+					title: 'STA',
+					dataIndex: 'STA',
+					key: 'STA',
+					render: (STA) => (STA !== null ? STA?.split('T')[1].slice(0, 5) : '-'),
+				}
+			: {
+					title: 'STD',
+					dataIndex: 'STD',
+					key: 'STD',
+					render: (STD) => (STD !== null ? STD?.split('T')[1].slice(0, 5) : '-'),
+				},
+		{ title: 'POS', dataIndex: 'pos', key: 'pos', render: (pos) => pos ?? '-' },
+		{
+			title: 'REG No.',
+			dataIndex: 'registration',
+			key: 'registration',
+			render: (registration) => registration ?? '-',
+		},
 		{
 			title: 'Actions',
 			key: 'actions',
@@ -276,13 +325,7 @@ const Seasonal = ({tab}) => {
 			<>
 				<div className="main_buttonContainer">
 					<div className="seasonal_container">
-						<Button
-							title="Create"
-							id="btn"
-							type="filledText"
-							isSubmit="submit"
-							onClick={openModal}
-						/>
+						<Button title="Create" id="btn" type="filledText" isSubmit="submit" onClick={openModal} />
 						<Button
 							id="btn"
 							title="Upload CSV"
@@ -310,7 +353,7 @@ const Seasonal = ({tab}) => {
 			key: '1',
 			label: 'Arrival',
 			children: Boolean(fetchedSeasonalPlans?.pages[0]?.data?.flightSchedule?.length) ? (
-				<Arrival data={seasonalData} columns={columns}  fetchData={fetchNextPage} pagination={hasNextPage} />
+				<Arrival data={seasonalData} columns={columns} fetchData={fetchNextPage} pagination={hasNextPage} />
 			) : (
 				noDataHandler()
 			),
@@ -319,7 +362,7 @@ const Seasonal = ({tab}) => {
 			key: '2',
 			label: 'Departure',
 			children: Boolean(fetchedSeasonalPlans?.pages[0]?.data?.flightSchedule?.length) ? (
-				<Departure data={seasonalData} columns={columns} fetchData={fetchNextPage} pagination={hasNextPage}/>
+				<Departure data={seasonalData} columns={columns} fetchData={fetchNextPage} pagination={hasNextPage} />
 			) : (
 				noDataHandler()
 			),
@@ -328,7 +371,7 @@ const Seasonal = ({tab}) => {
 
 	return (
 		<>
-		<PageLoader loading={isFetchLoading || isEditLoading || isPostLoading} />
+			<PageLoader loading={isFetchLoading || isEditLoading || isPostLoading || isDownloading} />
 			<div className="main_TableContainer">
 				<div className="top_container">
 					<div>
@@ -386,7 +429,12 @@ const Seasonal = ({tab}) => {
 					/>
 				</div>
 			</ModalComponent>
-			<UploadCsvModal isModalOpen={isCsvModalOpen} width="720px" closeModal={closeCsvModal} handleUpload={handleUpload} />
+			<UploadCsvModal
+				isModalOpen={isCsvModalOpen}
+				width="720px"
+				closeModal={closeCsvModal}
+				handleUpload={handleUpload}
+			/>
 			<ModalComponent
 				isModalOpen={isEditModalOpen}
 				width="80%"
@@ -400,7 +448,7 @@ const Seasonal = ({tab}) => {
 						handleButtonClose={handleCloseButton}
 						type={index}
 						initialValues={rowData}
-						isEdit = {true}
+						isEdit={true}
 					/>
 				</div>
 			</ModalComponent>
