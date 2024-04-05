0adb7b752f1165f11d8c63f51f20b324ab0e73a9
YogeshBalodi <Yogesh.b@antino.io>
2024-04-05 15:37:42 +0530

    done some changes in flight schedule table

--- src/views/afterAuth/dashboard/components/FlightSchedule.jsx
+++ src/views/afterAuth/dashboard/components/FlightSchedule.jsx
@@ -6,6 +6,7 @@ import TableComponent from '../../../../components/table/table';
 import CustomTypography from '../../../../components/typographyComponent/typographyComponent';
 import { useGetFlightScheduled } from '../../../../services/dashboard/flightSchedule/flightSchedule';
 import './style.scss';
+import { ConvertUtcToIst } from '../../../../utils';
 
 const FlightScheduleDataTab = ({ type }) => {
 	const [FlightScheduleData, setFlightScheduleData] = useState([]);
@@ -16,24 +17,38 @@ const FlightScheduleDataTab = ({ type }) => {
 			{ title: 'Flight', dataIndex: 'flightNumber', key: 'flightNumber' },
 			{ title: 'Status', dataIndex: 'status', key: 'status' },
 			{ title: 'ORG', dataIndex: 'org', key: 'org' },
-			{ title: 'VIA', dataIndex: 'via', key: 'via' },
 			{ title: 'STA', dataIndex: 'sta', key: 'sta' },
 			{ title: 'ETA', dataIndex: 'eta', key: 'eta' },
 			{ title: 'TMO', dataIndex: 'tmo', key: 'tmo' },
 			{ title: 'ATA', dataIndex: 'ata', key: 'ata' },
 			{ title: 'RNY', dataIndex: 'rny', key: 'rny' },
 			{ title: 'EOB', dataIndex: 'eob', key: 'eob' },
-			{ title: 'ONB', dataIndex: 'onBlock', key: 'onBlock' },
-			{ title: 'POS', dataIndex: 'pos', key: 'pos' },
-			{ title: 'Gate', dataIndex: 'gate', key: 'gate' },
-			{ title: 'Belt', dataIndex: 'belt', key: 'belt' },
+			{
+				title: 'ONB',
+				dataIndex: 'onBlock',
+				key: 'onBlock',
+				render: (text) => text && ConvertUtcToIst(text, 'YYYY/MM/DD'),
+			},
+			{
+				title: 'POS',
+				dataIndex: 'pos',
+				key: 'pos',
+				render: (_, record) => record?.resourceAllocation?.parkingStand,
+			},
+			{ title: 'Gate', dataIndex: 'gate', key: 'gate', render: (_, record) => record?.resourceAllocation?.gates },
+			{
+				title: 'Belt',
+				dataIndex: 'belt',
+				key: 'belt',
+				render: (_, record) => record?.resourceAllocation?.baggageBelt,
+			},
 			{ title: 'AC/ REGN', dataIndex: 'acRegn', key: 'acRegn' },
 			{ title: 'Call Sign', dataIndex: 'cs', key: 'cs' },
 			{ title: 'Remarks', dataIndex: 'remarks', key: 'remarks' },
 		];
 	}, [FlightScheduleData]);
 	useEffect(() => {
-		console.log('type data', type,data);
+		console.log('type data', type, data);
 		if (data?.pages) {
 			const lastPage = data.pages.length >= 1 ? data.pages[data.pages.length - 1] : [];
 			setFlightScheduleData([...FlightScheduleData, ...lastPage.data]);
