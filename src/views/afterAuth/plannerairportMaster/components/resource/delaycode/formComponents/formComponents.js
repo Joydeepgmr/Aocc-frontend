d1b6e4c8b757526e3e523f806ce51c8f263246ce
shaluAntino <shalu@antino.com>
2024-04-08 11:27:47 +0530

    fixed dalaycode dropdown

--- src/views/afterAuth/plannerairportMaster/components/resource/delaycode/formComponents/formComponents.js
+++ src/views/afterAuth/plannerairportMaster/components/resource/delaycode/formComponents/formComponents.js
@@ -4,10 +4,17 @@ import InputField from '../../../../../../../components/input/field/field';
 import Button from '../../../../../../../components/button/button';
 import Date from '../../../../../../../components/datapicker/datepicker';
 import CustomSelect from '../../../../../../../components/select/select';
-import './formComponents.scss'
+import './formComponents.scss';
 
-const FormComponent = ({handleSaveButton, handleButtonClose, initialValues, isEdit, isReadOnly, airlineDropdownData}) => {
-
+const FormComponent = ({
+	handleSaveButton,
+	handleButtonClose,
+	initialValues,
+	isEdit,
+	isReadOnly,
+	airlineDropdownData,
+}) => {
+	isEdit && (initialValues['airlineId'] = initialValues?.airline?.id);
 	const SelectAirlineData = useMemo(() => {
 		return airlineDropdownData?.map((data) => {
 			return { label: data.name, value: data.id };
@@ -31,48 +38,75 @@ const FormComponent = ({handleSaveButton, handleButtonClose, initialValues, isEd
 		form.setFieldsValue(initialValues);
 	}, [form, initialValues]);
 
-
 	return (
-			<Form form={form} layout="vertical" onFinish={onFinishHandler}>
-				<div className="delay_code_form_container">
-					<div className="delay_code_form_inputfields">
-						<InputField
-							label="Delay Code"
-							name="delayCode"
-							placeholder="Enter the airport name"
-							warning="Required field"
-							required
-							className="custom_input"
-							max="16"
-							disabled={isReadOnly || isEdit}
-						/>
+		<Form form={form} layout="vertical" onFinish={onFinishHandler}>
+			<div className="delay_code_form_container">
+				<div className="delay_code_form_inputfields">
+					<InputField
+						label="Delay Code"
+						name="delayCode"
+						placeholder="Enter the airport name"
+						warning="Required field"
+						required
+						className="custom_input"
+						max="16"
+						disabled={isReadOnly || isEdit}
+					/>
 
-						<InputField label="Group" name="group" placeholder="Filled Text" warning="Required field" className="custom_input" max="32" disabled={isReadOnly}/>
-						<CustomSelect
+					<InputField
+						label="Group"
+						name="group"
+						placeholder="Filled Text"
+						warning="Required field"
+						className="custom_input"
+						max="32"
+						disabled={isReadOnly}
+					/>
+					<CustomSelect
 						SelectData={SelectAirlineData}
 						label="Airline"
 						placeholder={'Select Airline'}
 						name="airlineId"
-						disabled={isReadOnly || isEdit}
+						disabled={isReadOnly}
 						className="select"
 					/>
-					</div>
-				
+				</div>
+
 				<Divider />
 				<div className="delay_code_form_inputfields">
-					
-						<Date label="Valid From" name="validFrom" placeholder="Enter the airport name" required className="custom_date" disabled={isReadOnly || isEdit}/>
-						<Date label="Valid To" name="validTill" placeholder="Enter the airport name" className="custom_date" disabled={isReadOnly}/>
+					<Date
+						label="Valid From"
+						name="validFrom"
+						placeholder="Enter the airport name"
+						required
+						className="custom_date"
+						disabled={isReadOnly || isEdit}
+					/>
+					<Date
+						label="Valid To"
+						name="validTill"
+						placeholder="Enter the airport name"
+						className="custom_date"
+						disabled={isReadOnly}
+					/>
 				</div>
-				</div>
-				<Divider />
-				{!isReadOnly && <div className="delay_code_form_inputfields">
+			</div>
+			<Divider />
+			{!isReadOnly && (
+				<div className="delay_code_form_inputfields">
 					<div className="form_bottomButton">
-						<Button title="Cancel" type="filledText" id="btn" className="custom_svgButton" onClick={handleButtonClose} />
+						<Button
+							title="Cancel"
+							type="filledText"
+							id="btn"
+							className="custom_svgButton"
+							onClick={handleButtonClose}
+						/>
 						<Button title="Save" type="filledText" id="btn" isSubmit="submit" />
 					</div>
-				</div>}
-			</Form>
+				</div>
+			)}
+		</Form>
 	);
 };
 
