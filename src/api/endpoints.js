3c139521a31752955b6a5a2a69735d1edf218863
YogeshBalodi <Yogesh.b@antino.io>
2024-04-15 16:46:05 +0530

    added / before  endpoints

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -7,15 +7,15 @@ export const GET_TIMELINE_GROUP_DATA = '/resource-allocation/get-resources';
 export const UPDATE_RESOURCE_ALLOCATION = '/resource-allocation/updateResource';
 
 /*------------- login --------------*/
-export const USER_LOGIN = 'user/login';
+export const USER_LOGIN = '/user/login';
 
 /*--------------Admin details -----------*/
 export const GET_ADMIN_DETAILS = 'user/userinfo';
 
 /*------------- IT admin airport Master --------------*/
-export const POST_LICENSE = 'airport-license';
-export const GET_LICENSE = 'airport-license/paginated/';
-export const GET_AIRPORT_NAME = 'global-airport/paginated';
+export const POST_LICENSE = '/airport-license';
+export const GET_LICENSE = '/airport-license/paginated/';
+export const GET_AIRPORT_NAME = '/global-airport/paginated';
 
 /*------------------ Seasonal Plans --------------------- */
 export const GET_SEASONAL_PLANS = '/seasonalPlan/getPlans';
