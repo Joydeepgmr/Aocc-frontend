91dc5c07024ea283fd8fb1efbe8222211a75c703
muskanAntino <muskan.k@antino.com>
2024-04-19 18:32:25 +0530

    cdm api integration

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -20,10 +20,10 @@ export const GET_AIRPORT_NAME = '/global-airport/paginated';
 /*------------------ Seasonal Plans --------------------- */
 export const GET_SEASONAL_PLANS = '/seasonalPlan/getPlans';
 export const POST_SEASONAL_PLANS = '/seasonalPlan/createPlan';
-export const EDIT_SEASONAL_PLANS_ARRIVAL =  '/seasonalPlan/arrival';
-export const EDIT_SEASONAL_PLANS_DEPARTURE =  'seasonalPlan/departure';
+export const EDIT_SEASONAL_PLANS_ARRIVAL = '/seasonalPlan/arrival';
+export const EDIT_SEASONAL_PLANS_DEPARTURE = 'seasonalPlan/departure';
 export const UPLOAD_CSV_BULK = '/seasonalPlan/bulkPlans';
-export const DOWNLOAD_CSV_TEMPLATE =  '/system-constant/';
+export const DOWNLOAD_CSV_TEMPLATE = '/system-constant/';
 
 /*------------- IT admin Global Master --------------*/
 export const GET_GLOBAL_AIRPORT = '/global-airport/paginated';
@@ -62,7 +62,7 @@ export const GET_TELEX_MESSAGE = '/telex/paginated';
 export const GET_CHECKIN_COUNTER = '/checkin-counter/paginated';
 export const CHECKIN_COUNTER = '/checkin-counter';
 export const GET_GATE = '/gate/paginated';
-export const GATE ='/gate';
+export const GATE = '/gate';
 export const GET_PARKING_STAND = '/parking-stand/paginated';
 export const PARKING_STAND = '/parking-stand';
 export const GET_TAXIWAY = '/taxiway/paginated';
@@ -108,4 +108,10 @@ export const GET_USER = '/bbs/paginated';
 export const USER = '/bbs';
 
 /*--------------------------Socket event name ---------------------*/
-export const SOCKET_EVENT_NAME = 'UPDATE_API'
+export const SOCKET_EVENT_NAME = 'UPDATE_API';
+
+/*--------------------------CDM ---------------------*/
+export const CDM_GET_ARRIVAL_DEPARTURE = '/cdmOps/';
+export const UPDATE_CDM_DATA = '/cdmOps';
+export const CDM_GET_TURN_AROUND = '/cdmOps/turnaround';
+export const UPDATE_CDM_TURN_AROUND = '/cdmOps/turnaround';
