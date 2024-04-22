715e442f0c73639560490878013c2bde716a0e57
Ankit Raj Gupta <ankit.g@antino.com>
2024-04-22 12:37:37 +0530

    Implemented socket in airport masters of IT admin flow

--- src/views/afterAuth/airportMasters/airportMasters.js
+++ src/views/afterAuth/airportMasters/airportMasters.js
@@ -4,8 +4,10 @@ import TopHeader from '../../../components/topHeader/topHeader';
 import { useCountriesDropdown, useGlobalAirportDropdown } from '../../../services';
 import { useGetLicenseData } from '../../../services/airportMasters/airportMasters';
 import CreateWrapper from '../globalMasters/components/createWrapper/createWrapper';
-import './airportMasters.scss';
 import LicenseSetupTable from './components/licenseSetupTable/licenseSetupTable';
+import SocketEventListener from '../../../socket/listner/socketListner';
+import { GET_LICENSE } from '../../../api';
+import './airportMasters.scss';
 
 const AirportMasters = () => {
 	const onError = ({
@@ -13,45 +15,48 @@ const AirportMasters = () => {
 			data: { message },
 		},
 	}) => toast.error(message);
-	const { data, isLoading, isFetching, hasNextPage, fetchNextPage } = useGetLicenseData({ onError });
+	const { data, isLoading, isFetching, hasNextPage, fetchNextPage, refetch: getAirportMastersRefetch } = useGetLicenseData({ onError });
 	const { data: airportDropdownData } = useGlobalAirportDropdown({ onError });
 	const { data: countryDropdownData } = useCountriesDropdown({ onError });
 	const [createProps, setCreateProps] = useState({ new: false, onDownload });
 
-	function onDownload() {}
+	function onDownload() { }
 
 	return (
-		<div className="airport_masters_container">
-			<div className="airport_master_header">
-				<TopHeader
-					heading="Airport Masters"
-					subHeading="Overview of airport licenses for the Airport Operating System"
-				/>
+		<>
+			<SocketEventListener refetch={getAirportMastersRefetch} apiName={GET_LICENSE} />
+			<div className="airport_masters_container">
+				<div className="airport_master_header">
+					<TopHeader
+						heading="Airport Masters"
+						subHeading="Overview of airport licenses for the Airport Operating System"
+					/>
+				</div>
+				<div>
+					<CreateWrapper
+						width="80%"
+						tableComponent={
+							<LicenseSetupTable
+								data={data}
+								createProps={createProps}
+								setCreateProps={setCreateProps}
+								fetchData={fetchNextPage}
+								pagination={{ isMore: hasNextPage }}
+								airportDropdownData={airportDropdownData}
+								countryDropdownData={countryDropdownData}
+								loading={isFetching}
+							/>
+						}
+						data={data?.pages}
+						createProps={createProps}
+						setCreateProps={setCreateProps}
+						label="New Airport License"
+						isLoading={isLoading}
+						isCsv={false}
+					/>
+				</div>
 			</div>
-			<div>
-				<CreateWrapper
-					width="80%"
-					tableComponent={
-						<LicenseSetupTable
-							data={data}
-							createProps={createProps}
-							setCreateProps={setCreateProps}
-							fetchData={fetchNextPage}
-							pagination={{ isMore: hasNextPage }}
-							airportDropdownData={airportDropdownData}
-							countryDropdownData={countryDropdownData}
-							loading={isFetching}
-						/>
-					}
-					data={data?.pages}
-					createProps={createProps}
-					setCreateProps={setCreateProps}
-					label="New Airport License"
-					isLoading={isLoading}
-					isCsv={false}
-				/>
-			</div>
-		</div>
+		</>
 	);
 };
 
