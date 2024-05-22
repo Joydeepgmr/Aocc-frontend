7fa82679b8091777512465417c0a09eaf708057f
YogeshBalodi <Yogesh.b@antino.io>
2024-05-22 13:12:04 +0530

    added a space to test deployment issue

--- src/app.jsx
+++ src/app.jsx
@@ -8,6 +8,7 @@ import Router from './routes/routes';
 
 const socketUrl = process.env.baseURL.split('/').slice(0, 3).join('/');
 const socket = io(socketUrl, { timeout: 10000, transports: ['websocket'] });
+
 export function App() {
 	const token = localStorage.getItem('_tid');
 	const userRole = localStorage.getItem('role');
