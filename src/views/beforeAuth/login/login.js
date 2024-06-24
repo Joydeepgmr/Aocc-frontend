fab608f329133ad87a13c5dec9b153a265081c91
YogeshBalodi <Yogesh.b@antino.io>
2024-06-24 16:17:12 +0530

    added dailyops and cdm access to planner

--- src/views/beforeAuth/login/login.js
+++ src/views/beforeAuth/login/login.js
@@ -32,6 +32,8 @@ export const Login = () => {
 					localStorage.setItem('cdmAccess', true);
 				}
 				if (data?.permission?.PLANNING_MGMNT?.CREATE) {
+					localStorage.setItem('dailyOpsAccess', true);
+					localStorage.setItem('cdmAccess', true);
 					localStorage.setItem('masterAccess', true);
 					localStorage.setItem('plannerAccess', true);
 					localStorage.setItem('subUserAccess', true);
